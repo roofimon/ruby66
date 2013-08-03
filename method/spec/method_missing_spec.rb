791f198217829fec2092fd8a746c624358cce7ec
Neng Liangpornrattana <punneng@gmail.com>
2013-08-04 01:12:22 +0700

    Move method_missing to spec folder

---
+++ method/spec/method_missing_spec.rb
@@ -0,0 +1,64 @@
+require_relative '../method_missing.rb'
+
+describe BookStore do
+  # Given
+  let!(:book_store) { BookStore.new }
+  let!(:ruby_book) { { title: 'Ruby', author: 'Matz'} }
+
+  before do
+    # Given
+    book_store.add_book('Ruby', 'Matz')
+  end
+
+  describe '#find_by_author' do
+    it 'should return a book writen by a given author' do
+      # When
+      book = book_store.find_by_author('Matz')
+      # Then
+      expect(book).to eq ruby_book
+    end
+
+    context 'When author does not exist' do
+      it 'should return nil' do
+        # When
+        book = book_store.find_by_author('Gosling')
+        # Then
+        expect(book).to be_nil
+      end
+    end
+  end
+
+  describe '#find_by_title' do
+    it 'should return a book that has a given title' do
+      # When
+      book = book_store.find_by_title('Ruby')
+      # Then
+      expect(book).to eq ruby_book
+    end
+
+    context 'When title does not exist' do
+      it 'should return nil' do
+        # When
+        book = book_store.find_by_title('Java')
+        # Then
+        expect(book).to be_nil
+      end
+    end
+  end
+
+  context 'When find by column that does not exist' do
+    it 'should return nil' do
+      # When
+      book = book_store.find_by_content("whatever")
+      # Then
+      expect(book).to be_nil
+    end
+  end
+
+  context 'When invoke a method that does not exist' do
+    it 'should raise NoMethodError' do
+      # When & Then
+      expect{book_store.find}.to raise_error(NoMethodError)
+    end
+  end
+end
