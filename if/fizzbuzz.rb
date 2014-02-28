bf571d9c1aa18159b803b5928e40502ae7ab6b60
roofimon <roofimon@gmail.com>
2014-02-28 07:12:37 +0700

    Finish fizzbuzz with if

--- if/fizzbuzz.rb
+++ if/fizzbuzz.rb
@@ -1,5 +1,9 @@
 class Fizzbuzz
   def count(number)
-    return number.to_s
+    result = number.to_s
+    result = "Fizz" if number%3 == 0
+    result = "Buzz" if number%5 == 0
+    result = "Fizzbuzz" if number%15 == 0
+    result
   end
 end
--- if/spec/fizzbuzz_spec.rb
+++ if/spec/fizzbuzz_spec.rb
@@ -5,4 +5,28 @@ describe 'Fizzbuzz' do
   it 'should return 1 if input is 1' do
     expect(fizzbuzz.count(1)).to eq '1'
   end
+
+  it 'should return 2 if input is 2' do
+    expect(fizzbuzz.count(2)).to eq '2'
+  end
+
+  it 'should return Fizz if input is 3' do
+    expect(fizzbuzz.count(3)).to eq 'Fizz'
+  end
+
+  it 'should return Buzz if input is 5' do
+    expect(fizzbuzz.count(5)).to eq 'Buzz'
+  end
+
+  it 'should return Fizz if input is 6' do
+    expect(fizzbuzz.count(6)).to eq 'Fizz'
+  end
+
+  it 'should return Buzz if input is 10' do
+    expect(fizzbuzz.count(10)).to eq 'Buzz'
+  end
+
+  it 'should return Fizzbuzz if input is 15' do
+    expect(fizzbuzz.count(15)).to eq 'Fizzbuzz'
+  end
 end
